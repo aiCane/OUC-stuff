e76a0f41d69514f3a9dcdfa6bdd2ee259ac8426d
yilin <zyl8820@stu.ouc.edu.cn>
2026-01-05 14:00:52 +0800

    [RDT 2.0] finished

--- TCP_Test/src/com/ouc/tcp/test/CheckSum.java
+++ TCP_Test/src/com/ouc/tcp/test/CheckSum.java
@@ -6,14 +6,22 @@ import com.ouc.tcp.message.TCP_HEADER;
 import com.ouc.tcp.message.TCP_PACKET;
 
 public class CheckSum {
-	
+
 	/*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
 	public static short computeChkSum(TCP_PACKET tcpPack) {
-		int checkSum = 0;
-		
-		
-		
-		return (short) checkSum;
+		short checkSum = 0;
+
+		CRC32 crc = new CRC32();
+		TCP_HEADER header = tcpPack.getTcpH();
+
+		crc.update(header.getTh_seq());
+		crc.update(header.getTh_ack());
+
+		for (int data : tcpPack.getTcpS().getData())
+			crc.update(data);
+
+		checkSum = (short) crc.getValue();
+		return checkSum;
 	}
-	
+
 }
