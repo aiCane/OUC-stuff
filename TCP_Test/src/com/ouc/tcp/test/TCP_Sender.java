2ffb236f2ce7bad3927fe59fd2f11abe5e4796f0
yilin <zyl8820@stu.ouc.edu.cn>
2026-01-13 21:11:35 +0800

    [TCP Tahoe] finished

--- TCP_Test/src/com/ouc/tcp/test/TCP_Sender.java
+++ TCP_Test/src/com/ouc/tcp/test/TCP_Sender.java
@@ -6,9 +6,7 @@ package com.ouc.tcp.test;
 import com.ouc.tcp.client.*;
 import com.ouc.tcp.message.*;
 
-import java.util.TimerTask;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 public class TCP_Sender extends TCP_Sender_ADT {
 
@@ -17,7 +15,7 @@ public class TCP_Sender extends TCP_Sender_ADT {
 	// private UDT_RetransTask retransTask;
 
 	/* [GBN] */
-	private final int windowSize = 8; // N
+	// private final int windowSize = 8; // N
 	private int base = 1;
 	private int nextSeqNum = 1;
 	// private TCP_PACKET[] windowPackets = new TCP_PACKET[windowSize];
@@ -25,6 +23,16 @@ public class TCP_Sender extends TCP_Sender_ADT {
 	/* [SR] */
 	private Map<Integer, TCP_PACKET> sendWindow = new HashMap<>();
 	private Map<Integer, Boolean> ackMap = new HashMap<>();
+
+	/* [TCP Tahoe] */
+	private int cwnd = 1;
+	private int ssthresh = 16;
+	private int count = 0;
+	private List<int[]> appBuffer = new ArrayList<>();
+
+	/* [TCP Reno] */
+	private int lastACK = -1;
+	private int dupAckCount = 0;
 
 	// private final int APP_DATA_LENGTH = 100;
 
@@ -37,30 +45,9 @@ public class TCP_Sender extends TCP_Sender_ADT {
 	@Override
 	//可靠发送（应用层调用）：封装应用层数据，产生TCP数据报；需要修改
 	public void rdt_send(int dataIndex, int[] appData) {
-		if (nextSeqNum > (base + windowSize - 1)) { // [GBN] [base, base + windowSize - 1]
-			System.out.print(".");
-			return;
-		}
-
-		// 生成TCP数据报（设置序号和数据字段/校验和),注意打包的顺序 appData.length == 100
-		TCP_HEADER localH = new TCP_HEADER();
-		localH.setTh_seq(nextSeqNum); // [GBN] 包序号设置为字节流号：
-		TCP_SEGMENT localS = new TCP_SEGMENT();
-		localS.setData(appData);
-		TCP_PACKET localP = new TCP_PACKET(localH, localS, destinAddr);
-		localH.setTh_sum(CheckSum.computeChkSum(localP));
-		localP.setTcpH(localH);
-
-		//发送TCP数据报
-		// windowPackets[nextSeqNum % windowSize] = localP;
-		sendWindow.put(nextSeqNum, localP);
-		ackMap.put(nextSeqNum, false);
-		localH.setTh_eflag((byte)7);
-		udt_send(localP);
-
-		if (base == nextSeqNum) resetTimer(); // [RDT 3.0]
-
-		nextSeqNum++;
+		appBuffer.add(appData);
+		/* [GBN] [base, base + windowSize - 1]; [TCP Tahoe] */
+		trySend();
 	}
 
 	@Override
@@ -73,6 +60,30 @@ public class TCP_Sender extends TCP_Sender_ADT {
 		client.send(stcpPack);
 	}
 
+	private void trySend() {
+		// 只要缓冲区不为空，且窗口还有空间
+		while (!appBuffer.isEmpty() && nextSeqNum < base + cwnd) {
+			int[] data = appBuffer.remove(0);
+
+			// 生成TCP数据报（设置序号和数据字段/校验和),注意打包的顺序 appData.length == 100
+			TCP_HEADER localH = new TCP_HEADER();
+			localH.setTh_seq(nextSeqNum); // [GBN] 包序号设置为字节流号：
+			TCP_SEGMENT localS = new TCP_SEGMENT();
+			localS.setData(data);
+			TCP_PACKET localP = new TCP_PACKET(localH, localS, destinAddr);
+			localH.setTh_sum(CheckSum.computeChkSum(localP));
+			localP.setTcpH(localH);
+
+			// 发送与记录
+			sendWindow.put(nextSeqNum, localP);
+			ackMap.put(nextSeqNum, false);
+			udt_send(localP);
+
+			if (base == nextSeqNum) resetTimer(); // [RDT 3.0]
+			nextSeqNum++;
+		}
+	}
+
 	@Override
 	//需要修改
 	public void waitACK() {
@@ -83,21 +94,31 @@ public class TCP_Sender extends TCP_Sender_ADT {
 		int currentAck = ackQueue.poll();
 
 		/* [GBN] 累计确认是 if (currentAck >= base) 执行逻辑 */
-		// if (currentAck < base) return; // 这里条件取反可减少缩进
-		if (currentAck < nextSeqNum) ackMap.put(currentAck, true);
+		/* if (currentAck < base) return; 这里条件取反可减少缩进 */
+		if (currentAck < nextSeqNum && !ackMap.get(currentAck)) {
+			ackMap.put(currentAck, true);
 
+			if (cwnd < ssthresh) {
+				cwnd++;
+			} else if (++count >= cwnd) {
+				cwnd++;
+				count = 0;
+			}
+
+		}
+
+		boolean windowSlided = false;
 		while (ackMap.containsKey(base) && ackMap.get(base)) {
 			sendWindow.remove(base);
 			ackMap.remove(base);
 			base++;
+			windowSlided = true;
 
 			if (base < nextSeqNum) { resetTimer(); }
 			else { freeTimer(); }
 		}
-		// base = currentAck + 1;
 
-		// if (base == nextSeqNum) { freeTimer(); } // [RDT 3.0]
-		// else if (base < nextSeqNum) { resetTimer(); }
+		if (windowSlided) { trySend(); }
 	}
 
 	/* [RDT 3.0] */
@@ -107,9 +128,14 @@ public class TCP_Sender extends TCP_Sender_ADT {
 		TimerTask timeout = new TimerTask() {
 			@Override
 			public void run() {
+				// TCP_PACKET p = sendWindow.get(base);
+				// if (p == null) return;
+				// udt_send(p);
+				ssthresh = Math.max(cwnd / 2, 2);
+				cwnd = 1;
+
 				TCP_PACKET p = sendWindow.get(base);
-				if (p == null) return;
-				udt_send(p);
+				if (p != null) udt_send(p);
 			}
 		};
 		timer.schedule(timeout, 2000, 1000);
@@ -127,8 +153,8 @@ public class TCP_Sender extends TCP_Sender_ADT {
 	public void recv(TCP_PACKET recvPack) {
 		System.out.println("Receiving...");
 		if (
-			CheckSum.computeChkSum(recvPack) != recvPack.getTcpH().getTh_sum() // ||
-			// recvPack.getTcpH().getTh_ack() < base
+			CheckSum.computeChkSum(recvPack) != recvPack.getTcpH().getTh_sum() ||
+			recvPack.getTcpH().getTh_ack() < base
 		) { return; }
 		System.out.println("Received :)");
 		ackQueue.add(recvPack.getTcpH().getTh_ack());
